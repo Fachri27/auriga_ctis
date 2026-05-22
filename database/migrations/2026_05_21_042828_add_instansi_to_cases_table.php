f6ecd970d94ddcce20060c80b6d25d8a602372ac
Fachri <aiti@AITIs-MacBook-Air-2.local>
2026-05-22 16:47:38 +0700

    pahri

--- database/migrations/2026_05_21_042828_add_instansi_to_cases_table.php
+++ database/migrations/2026_05_21_042828_add_instansi_to_cases_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('cases', function (Blueprint $table) {
+            $table->longText('status_narasi')->nullable();
             $table->longText('instansi')->nullable()->after('status_narasi');
         });
     }
