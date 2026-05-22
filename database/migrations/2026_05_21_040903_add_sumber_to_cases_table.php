421478cc0e5ecd8c7b43ae5111451298776c2699
Fachri <aiti@AITIs-MacBook-Air-2.local>
2026-05-22 16:46:53 +0700

    pahri

--- database/migrations/2026_05_21_040903_add_sumber_to_cases_table.php
+++ database/migrations/2026_05_21_040903_add_sumber_to_cases_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('cases', function (Blueprint $table) {
+            $table->longText('status_narasi')->nullable();
             $table->longText('sumber')->nullable()->after('terlapor');
 
         });
